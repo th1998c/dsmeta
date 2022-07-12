1ed6d068a71f2187eef2298b13f364328213a878
Thiago Carneiro da Silva <thiagocarneiro1998@gmail.com>
2022-07-11 22:23:04 -0300

    Other components

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import logo from '../../assets/img/logo.svg'
+import './styles.css'
+function Header() {
+    return (
+        <header>
+        <div className="dsmeta-logo-container">
+            <img src={logo} alt="DSMeta"/>
+            <h1>DSMeta</h1>
+            <p>Desenvolvido por <a href="https://www.instagram.com/thcs98/" target="_blank">@thcs98</a></p>
+        </div>
+    </header>
+    )
+}
+
+export default Header
